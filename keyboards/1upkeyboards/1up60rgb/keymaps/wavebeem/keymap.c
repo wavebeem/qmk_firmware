118e3adabb1efc6b458d552fb6489ca7fcf8f827
Brian Mock <brian@mockbrian.com>
2018-10-20 00:01:58 -0700

    keymap stuff

--- keyboards/1upkeyboards/1up60rgb/keymaps/wavebeem/keymap.c
+++ keyboards/1upkeyboards/1up60rgb/keymaps/wavebeem/keymap.c
@@ -26,10 +26,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, MO(1), KC_RALT, KC_RGUI, MO(2)),
 
 	[1] = LAYOUT_60_ansi(
-		KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______,
-		_______, _______, KC_UP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		_______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-		KC_LSFT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+		KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
+		_______, _______, KC_PGUP, _______, _______, _______, _______, _______, _______, _______, KC_UP, _______, _______, KC_BSPC,
+		_______, KC_HOME, KC_PGDN, KC_END, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,
+		_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______),
 
   [2] = LAYOUT_60_ansi(
@@ -39,8 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		_______, _______, _______, RGB_VAD, RGB_VAI, RGB_SAI, RGB_HUD, RGB_HUI, RGB_MOD, RGB_TOG, _______, _______,
 		_______, _______, _______, _______, _______, _______, _______, _______),
 };
-m;
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
-
