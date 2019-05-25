2f44915d977a0ad82a99ed3375028b663f7efa1c
Brian Mock <brian@mockbrian.com>
2019-05-25 10:52:20 -0700

    go back...

--- keyboards/planck/keymaps/wavebeem/keymap.c
+++ keyboards/planck/keymaps/wavebeem/keymap.c
@@ -1,19 +1,11 @@
 #include QMK_KEYBOARD_H
 
-#define $__Z    LCTL_T(KC_Z)
-#define $__X    LALT_T(KC_X)
-#define $__C    LGUI_T(KC_C)
-/* ---------------------- */
-#define $__COMM RGUI_T(KC_COMM)
-#define $__DOT  RALT_T(KC_DOT)
-#define $__SLSH RCTL_T(KC_SLSH)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_planck_grid(
     KC_ESC  , KC_Q    , KC_W    , KC_E    , KC_R   , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P     , KC_BSPC ,
     KC_TAB  , KC_A    , KC_S    , KC_D    , KC_F   , KC_G    , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN  , KC_QUOT ,
-    KC_LSFT , $__Z    , $__X    , $__C    , KC_V   , KC_B    , KC_N    , KC_M    , $__COMM , $__DOT  , $__SLSH  , KC_RSFT ,
+    KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V   , KC_B    , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH  , KC_RSFT ,
     KC_LCTL , KC_LALT , KC_LGUI , MO(4)   , MO(1)  , KC_ENT  , KC_SPC  , MO(2)   , MO(3)   , KC_RGUI , KC_RALT  , KC_RCTL
   ),
 
