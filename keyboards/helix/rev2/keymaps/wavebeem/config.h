15f8776e568718ccd184571d8263497c489c38fc
Brian Mock <brian@mockbrian.com>
2019-08-24 20:03:49 -0700

    idk

---
+++ keyboards/helix/rev2/keymaps/wavebeem/config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#undef HELIX_ROWS
+#define HELIX_ROWS 4
+
+/* Use I2C or Serial, not both */
+// #define USE_SERIAL
+// #define USE_I2C
+
+/* Select hand configuration */
+// #define MASTER_LEFT
+// #define _MASTER_RIGHT
+// #define EE_HANDS
