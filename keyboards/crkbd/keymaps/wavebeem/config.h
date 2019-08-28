daff689f04488278ed441863950d0e8f6208f076
Brian Mock <brian@mockbrian.com>
2019-08-27 18:43:31 -0700

    byebye

--- keyboards/crkbd/keymaps/wavebeem/config.h
+++
@@ -1,5 +0,0 @@
-// Why is my left half suddenly unable to be used as the master half? The right
-// half is super laggy and frequently the keys don't even register if I click
-// them. And worse, sometimes they get stuck down for a couple seconds! It's
-// completely unusable, unfortunately.
-#define MASTER_RIGHT
