46e1f3cab15455418561dd52d8af1ebe2d581022
Brian Mock <brian@wavebeem.com>
2020-11-16 13:26:38 -0800

    keebin

--- keyboards/helix/rev2/keymaps/wavebeem/keymap.c
+++ keyboards/helix/rev2/keymaps/wavebeem/keymap.c
@@ -34,10 +34,18 @@ enum custom_keycodes {
     RGBRST = SAFE_RANGE
 };
 
+enum Layers {
+    LAYER_BASE = 0,
+    LAYER_NUM,
+    LAYER_NAV,
+    LAYER_GAMES,
+    LAYER_MEDIA
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // ########## BASE LAYER #######################################################################################################################
-[0] = LAYOUT(
+[LAYER_BASE] = LAYOUT(
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
      KC_ESC  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,                     KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSPC ,
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
@@ -45,12 +53,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
      KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    ,                     KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_ENT  ,
 // ,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,
-     MO(4)   , KC_LCTL , KC_LALT , KC_LGUI , MO(2)   , KC_RSFT , KC_SPC  , TG(3)   , KC_SPC  , MO(1)   , KC_RGUI , KC_RALT , KC_RCTL , KC_RSFT
+     MO(4)   , KC_LCTL , KC_LALT , KC_LGUI , MO(2)   , KC_RSFT , KC_SPC  , KC_F12  , KC_SPC  , MO(1)   , KC_RGUI , KC_RALT , KC_RCTL , KC_RSFT
 // ,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,
 ),
 
 // ########## NUMBERS & SYMBOLS LAYER ##########################################################################################################
-[1] = LAYOUT(
+[LAYER_NUM] = LAYOUT(
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
      _______ , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    ,                     KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , _______ ,
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
@@ -63,33 +71,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 // ########## ARROWS & F1-F12 LAYER ############################################################################################################
-[2] = LAYOUT(
+[LAYER_NAV] = LAYOUT(
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
-     _______ , KC_TAB  , KC_HOME , KC_UP   , KC_END  , KC_ENT  ,                     KC_F13  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , _______ ,
+     _______ , KC_TAB  , KC_HOME , KC_UP   , KC_END  , KC_LSFT ,                     KC_F13  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , _______ ,
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
      _______ , KC_LSFT , KC_LEFT , KC_DOWN , KC_RGHT , KC_ENT  ,                     KC_F14  , KC_F5   , KC_F6   , KC_F7   , KC_F8  , KC_DEL  ,
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
      _______ , KC_LSFT , KC_SPC  , KC_PGUP , KC_PGDN , KC_ENT  ,                     KC_F15  , KC_F9   , KC_F10  , KC_F11  , KC_F12  , _______ ,
 // ,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,
-     _______ , _______ , _______ , _______ , _______ , _______ , KC_CAPS , RESET   , _______ , _______ , _______ , _______ , _______ , _______
+     _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______
 // ,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,
 ),
 
 // ########## GAMES LAYER ######################################################################################################################
-[3] = LAYOUT(
+[LAYER_GAMES] = LAYOUT(
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
-     _______ , _______ , _______ , _______ , _______ , _______ ,                     KC_F1   , KC_HOME , KC_UP   , KC_END  , KC_F4   , KC_BSPC ,
+     _______ , _______ , _______ , _______ , _______ , _______ ,                     KC_F13  , KC_HOME , KC_UP   , KC_END  , KC_F16  , KC_BSPC ,
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
-     _______ , _______ , _______ , _______ , _______ , _______ ,                     KC_F2   , KC_LEFT , KC_DOWN , KC_RGHT , KC_F5   , KC_DEL  ,
+     _______ , _______ , _______ , _______ , _______ , _______ ,                     KC_F14  , KC_LEFT , KC_DOWN , KC_RGHT , KC_F17  , KC_DEL  ,
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
-     _______ , _______ , _______ , _______ , _______ , _______ ,                     KC_F3   , KC_PGDN , KC_PGUP , KC_ENT  , KC_F6   , KC_ENT ,
+     _______ , _______ , _______ , _______ , _______ , _______ ,                     KC_F15  , KC_PGDN , KC_PGUP , KC_ENT  , KC_F18  , KC_ENT ,
 // ,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,
-     KC_LALT , KC_LGUI , KC_LALT , KC_LCTL , KC_SPC  , KC_SPC  , KC_SPC  , _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
+     _______ , KC_LGUI , KC_LALT , KC_LCTL , KC_SPC  , KC_SPC  , KC_SPC  , _______ , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
 // ,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,
 ),
 
 // ########## LED & MEDIA KEYS LAYER ###########################################################################################################
-[4] = LAYOUT(
+[LAYER_MEDIA] = LAYOUT(
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
      XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,                     XXXXXXX , RGB_HUD , RGB_HUI , RGB_TOG , XXXXXXX , XXXXXXX ,
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
@@ -97,7 +105,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
      XXXXXXX , KC_MPRV , KC_MNXT , KC_MPLY , XXXXXXX , XXXXXXX ,                     XXXXXXX , RGB_SAD , RGB_SAI , RGB_RMOD, XXXXXXX , XXXXXXX ,
 // ,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,
-     XXXXXXX , KC_LSFT , RGBRST  , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
+     XXXXXXX , KC_LSFT , RGBRST  , XXXXXXX , XXXXXXX , XXXXXXX , RESET   , TG(3)   , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_CAPS
 // ,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,
 ),
 };
@@ -250,6 +258,46 @@ static const char PROGMEM name[] = {
 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
 
+static const char PROGMEM games[] = {
+// 'helix-screen-game', 128x32px
+0x00, 0x00, 0xf0, 0xf0, 0x3c, 0x3c, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc,
+0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc,
+0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc,
+0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc,
+0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc,
+0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc,
+0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc,
+0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0x3c, 0x3c, 0xf0, 0xf0, 0x00, 0x00,
+0x00, 0x00, 0xff, 0xff, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xc0, 0xc0, 0x3f, 0x3f,
+0xff, 0xff, 0x3f, 0x3f, 0xc0, 0xc0, 0xff, 0xff, 0x0c, 0x0c, 0xff, 0xff, 0x3f, 0x3f, 0xcf, 0xcf,
+0xcf, 0xcf, 0x00, 0x00, 0xff, 0xff, 0x3f, 0x3f, 0xcf, 0xcf, 0xcf, 0xcf, 0x3f, 0x3f, 0xff, 0xff,
+0x3f, 0x3f, 0xcf, 0xcf, 0xcf, 0xcf, 0x3f, 0x3f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+0x03, 0x03, 0xfc, 0xfc, 0x3c, 0x3c, 0x3c, 0x3c, 0x3f, 0x3f, 0xff, 0xff, 0xff, 0xff, 0xcf, 0xcf,
+0xcf, 0xcf, 0x3f, 0x3f, 0xff, 0xff, 0x3f, 0x3f, 0xcf, 0xcf, 0x3f, 0x3f, 0xcf, 0xcf, 0x3f, 0x3f,
+0xff, 0xff, 0x3f, 0x3f, 0xcf, 0xcf, 0xcf, 0xcf, 0x3f, 0x3f, 0xff, 0xff, 0x3f, 0x3f, 0xcf, 0xcf,
+0xcf, 0xcf, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00,
+0x00, 0x00, 0xff, 0xff, 0x00, 0x00, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfc, 0xfc,
+0xc3, 0xc3, 0xfc, 0xfc, 0xff, 0xff, 0xff, 0xff, 0xc0, 0xc0, 0xff, 0xff, 0xf0, 0xf0, 0xcf, 0xcf,
+0xcf, 0xcf, 0xc0, 0xc0, 0xff, 0xff, 0xf0, 0xf0, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xff, 0xff,
+0xf0, 0xf0, 0xcf, 0xcf, 0xcf, 0xcf, 0xf0, 0xf0, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+0xf0, 0xf0, 0xcf, 0xcf, 0xcf, 0xcf, 0xcf, 0xcf, 0xf0, 0xf0, 0xff, 0xff, 0xf3, 0xf3, 0xcc, 0xcc,
+0xcc, 0xcc, 0xc0, 0xc0, 0xff, 0xff, 0xc0, 0xc0, 0xff, 0xff, 0xfc, 0xfc, 0xff, 0xff, 0xc0, 0xc0,
+0xff, 0xff, 0xf0, 0xf0, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xcc, 0xff, 0xff, 0xcf, 0xcf, 0xcc, 0xcc,
+0xcc, 0xcc, 0xf3, 0xf3, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00,
+0x00, 0x00, 0x0f, 0x0f, 0x3c, 0x3c, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33,
+0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33,
+0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33,
+0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33,
+0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33,
+0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33,
+0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33,
+0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x33, 0x3c, 0x3c, 0x0f, 0x0f, 0x00, 0x00
+};
+
+static void render_games(void) {
+    oled_write_raw_P(games, sizeof games);
+}
+
 static void render_logo1(void) {
     oled_write_raw_P(logo1, sizeof logo1);
 }
@@ -289,7 +337,9 @@ void oled_task_user(void) {
         last_image_swap = timer_read();
         image_swap = !image_swap;
     }
-    if (is_master) {
+    if (IS_LAYER_ON(LAYER_GAMES)){
+        render_games();
+    } else if (is_master) {
         render_logo();
     } else {
         render_status();
