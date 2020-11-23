fa255c4bbeb1291e6d01edd1520172c94b9f3a9c
Brian Mock <brian@wavebeem.com>
2020-11-23 09:32:27 -0800

    blah

--- keyboards/helix/rev2/keymaps/wavebeem/keymap.c
+++ keyboards/helix/rev2/keymaps/wavebeem/keymap.c
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // ,---------,---------,---------,---------,---------,---------,                   ,---------,---------,---------,---------,---------,---------,
      KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    ,                     KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_ENT  ,
 // ,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,
-     L_MEDIA , KC_LCTL , KC_LALT , KC_LGUI , L_NAV   , KC_RSFT , KC_SPC  , KC_F12  , KC_SPC  , L_NUM   , KC_RGUI , KC_RALT , KC_RCTL , KC_RSFT
+     L_MEDIA , KC_LCTL , KC_LALT , KC_LGUI , L_NAV   , KC_RSFT , KC_SPC  , KC_F12  , KC_SPC  , L_NUM   , KC_RGUI , KC_RALT , KC_RCTL , KC_F10
 // ,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,---------,
 ),
 
