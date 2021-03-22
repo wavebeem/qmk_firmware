b3a96825d2fcaf12f9e9311bb63c5c57ad4e06a1
Brian Mock <brian@wavebeem.com>
2021-03-22 09:11:40 -0700

    fix some OLED flicker

--- keyboards/helix/rev2/keymaps/wavebeem/keymap.c
+++ keyboards/helix/rev2/keymaps/wavebeem/keymap.c
@@ -133,13 +133,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
-    case LAYER_BASE:
-    case LAYER_GAMES:
-        oled_scroll_off();
-        oled_clear();
-        break;
-    }
+    #ifdef OLED_DRIVER_ENABLE
+        // Entering or exiting games layer
+        if (get_highest_layer(state) == LAYER_GAMES || IS_LAYER_ON(LAYER_GAMES)) {
+            oled_scroll_off();
+            oled_clear();
+        }
+    #endif
     return state;
 }
 
